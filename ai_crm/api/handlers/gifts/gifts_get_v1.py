3315912cdeefc8f35063780d0dfc1800c8744a64
timofeytst <yarik.mist@gmail.com>
2025-09-21 11:34:46 +0300

    lint

--- ai_crm/api/handlers/gifts/gifts_get_v1.py
+++ ai_crm/api/handlers/gifts/gifts_get_v1.py
@@ -3,7 +3,4 @@ from ai_crm.pkg.context import web_context
 from ai_crm.pkg.models.ai_crm import gift as gift_models
 
 async def handle(context: web_context.WebContext) -> list[gift_models.Gift]:
-    gifts = await gifts_service.get_gifts(context)
-    # users_dict = [user.to_json_dict() for user in users]
-
-    return gifts
+    return await gifts_service.get_gifts(context)
